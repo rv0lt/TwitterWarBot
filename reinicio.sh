cff4d30aa2e110176cbd46f9cd460312197cbdc3
Alvaro Revuelta <arevuelta@Rv0lt>
2020-06-16 01:33:40 +0200

    reinicio

--- reinicio.sh
+++ reinicio.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-sed 's/0/3/' jugadores.txt > s.txt 
+sed 's/0/5/' jugadores.txt > s.txt 
 mv s.txt jugadores.txt
-sed 's/1/3/' jugadores.txt > s.txt 
+sed 's/1/5/' jugadores.txt > s.txt 
 mv s.txt jugadores.txt
 echo "1\n0\n0\n0" > contador.txt
 echo "" > muertos.txt
